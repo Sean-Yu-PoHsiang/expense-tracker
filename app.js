82fbe6b306144bc63fe0cfa261c38298b3b4b66b
Sean Yu <canvaslike@gmail.com>
2020-10-06 02:47:48 +0800

    feat: add mongoose and seeders

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+//require node modules
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 const PORT = 3000
 
+//mongodb connection setting
+const MONGODB_URI = 'mongodb://localhost/expense-tracker'
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+//connection error
+db.on('error', () => {
+  console.log('mongodb error')
+})
+//connection success
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
+//route of home page
 app.get('/', (req, res) => {
   res.send('expense tracker')
 })
 
+//server listen to localhost:3000
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
---
+++ models/seeds/categorySeeder.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const Category = require('../category.js')
+
+const categoryList = [
+  {
+    category: "家居物業",
+    icon: "fas fa-home"
+  },
+  {
+    category: "交通旅行",
+    icon: "fas fa-shuttle-van"
+  },
+  {
+    category: "休閒娛樂",
+    icon: "fas fa-grin-beam"
+  },
+  {
+    category: "餐飲食品",
+    icon: "fas fa-utensils"
+  },
+  {
+    category: "其他",
+    icon: "fas fa-pen"
+  },
+]
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+  Category.insertMany(categoryList)
+  console.log('categorySeeder done')
+})
