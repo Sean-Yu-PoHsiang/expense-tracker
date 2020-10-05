98a2eea84e9185eed8b6f5f3d6f3a5600b805152
Sean Yu <canvaslike@gmail.com>
2020-10-06 03:41:55 +0800

    feat: modify index.handlebars

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
 
+const Record = require('./models/record.js')
+
 //mongodb connection setting
 const MONGODB_URI = 'mongodb://localhost/expense-tracker'
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -29,7 +31,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 //route of home page
 app.get('/', (req, res) => {
-  res.render('index')
+  let totalAmount = 0
+  return Record.find()
+    .lean()
+    .then(records => {
+      records.forEach(record => totalAmount += record.amount)
+      return records
+    })
+    .then(records => res.render('index', { records, totalAmount }))
+    .catch((err) => res.status(err).send(err).then(console.log(err)))
 })
 
 //server listen to localhost:3000
