6893b54fdfbd8defb0019a85ba457d3a64e96196
Sean Yu <canvaslike@gmail.com>
2020-10-06 17:13:21 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -52,13 +52,44 @@ app.get('/new', (req, res) => {
     .catch((err) => res.status(err).send(err).then(console.log(err)))
 })
 
-//route of post new record
+//route of post new record to data-base
 app.post('/new', (req, res) => {
   const newRecord = req.body
   return Record.create(newRecord)
     .then(() => res.redirect('/'))
     .catch((err) => res.status(err).send(err).then(console.log(err)))
 })
+
+//route of edit page
+app.get('/edit/:id', (req, res) => {
+  const id = req.params.id
+  return Category.find()
+    .lean()
+    .then(categories => {
+      Record.findById(id)
+        .lean()
+        .then(record => res.render('edit', { categories, record }))
+        .catch((err) => res.status(err).send(err).then(console.log(err)))
+    })
+    .catch((err) => res.status(err).send(err).then(console.log(err)))
+})
+
+//route of post edit 
+app.post('/edit/:id', (req, res) => {
+  const id = req.params.id
+  const reqBody = req.body
+  return Record.findById(id)
+    .then(record => {
+      record.name = reqBody.name
+      record.date = reqBody.date
+      record.category = reqBody.category
+      record.amount = reqBody.amount
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch((err) => res.status(err).send(err).then(console.log(err)))
+})
+
 
 //server listen to localhost:3000
 app.listen(PORT, () => {
