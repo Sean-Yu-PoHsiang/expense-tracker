b587995a02b9727bbe57dd44193d8ae29baa79dc
Sean Yu <canvaslike@gmail.com>
2020-10-10 02:37:32 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 
 const app = express()
 const PORT = 3000
@@ -32,6 +33,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 //set public file
 app.use(express.static('public'))
+
+//set method override
+app.use(methodOverride('_method'))
 
 //route of home page
 app.get('/', (req, res) => {
@@ -108,7 +112,7 @@ app.get('/edit/:id', (req, res) => {
 })
 
 //route of post edit 
-app.post('/edit/:id', (req, res) => {
+app.put('/edit/:id', (req, res) => {
   const id = req.params.id
   const reqBody = req.body
   let temp = req.body.category.split('|')
@@ -126,7 +130,7 @@ app.post('/edit/:id', (req, res) => {
 })
 
 //route of delete record
-app.post('/delete/:id', (req, res) => {
+app.delete('/delete/:id', (req, res) => {
   const filterCategory = req.query.category
   const id = req.params.id
   return Record.findById(id)
